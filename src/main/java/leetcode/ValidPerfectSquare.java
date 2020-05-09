5d1db24009b5a63b0237ffea094dc5256aa971ab
Varun Gupta <varun.gupta04@nagarro.com>
2020-05-09 16:29:58 +0530

    Valid Perfect Square

---
+++ src/main/java/leetcode/ValidPerfectSquare.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/*
+* Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false
+*
+* */
+
+public class ValidPerfectSquare {
+
+    public boolean isPerfectSquare(int num) {
+
+
+        if(num == 1)
+            return true;
+        else {
+            int i = 1;
+            if(num %2 == 0) {
+                i=2;
+            } else {
+                i=3;
+            }
+            for(;i<=num/2; i+=2) {
+                if((num % (i*i)) == 0) {
+                    return isPerfectSquare(num/(i*i));
+                }
+            }
+            return false;
+        }
+    }
+}
