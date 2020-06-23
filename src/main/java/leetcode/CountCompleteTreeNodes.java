0d259a31d978590d66716593cbd87c54b8b876c4
Varun Gupta <varun.gupta04@nagarro.com>
2020-06-23 18:36:58 +0530

    Count Complete Tree Nodes

---
+++ src/main/java/leetcode/CountCompleteTreeNodes.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+/*  222. Count Complete Tree Nodes  */
+
+/*  https://leetcode.com/problems/count-complete-tree-nodes/    */
+
+/*
+*
+* Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input:
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+*
+* */
+
+
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if(null == root) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+    }
+
+    static  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
