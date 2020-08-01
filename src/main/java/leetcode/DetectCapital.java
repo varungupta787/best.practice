7da88864ef6531c94b4896a926d987ecb2d42c0e
Varun Gupta <varun.gupta04@nagarro.com>
2020-08-01 17:48:30 +0530

    Detect Capital

---
+++ src/main/java/leetcode/DetectCapital.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+/*  520. Detect Capital */
+
+/*  https://leetcode.com/problems/detect-capital    */
+
+/*
+*
+* Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+
+Example 1:
+
+Input: "USA"
+Output: True
+
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+*
+* */
+
+public class DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        return word.matches("[A-Z]*|.[a-z]*");
+    }
+}
+
+/*
+* -------------------------- Regex Helper  ------------------------
+Character classes
+.	any character except newline
+\w\d\s	word, digit, whitespace
+\W\D\S	not word, digit, whitespace
+[abc]	any of a, b, or c
+[^abc]	not a, b, or c
+[a-g]	character between a & g
+Anchors
+^abc$	start / end of the string
+\b\B	word, not-word boundary
+Escaped characters
+\.\*\\	escaped special characters
+\t\n\r	tab, linefeed, carriage return
+Groups & Lookaround
+(abc)	capture group
+\1	backreference to group #1
+(?:abc)	non-capturing group
+(?=abc)	positive lookahead
+(?!abc)	negative lookahead
+Quantifiers & Alternation
+a*a+a?	0 or more, 1 or more, 0 or 1
+a{5}a{2,}	exactly five, two or more
+a{1,3}	between one & three
+a+?a{2,}?	match as few as possible
+ab|cd	match ab or cd
+*
+* */
