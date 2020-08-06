ce52be71dbcdbaade89c187c4718dab098fe5032
Varun Gupta <varun.gupta04@nagarro.com>
2020-08-06 22:37:48 +0530

    Best Time to Buy and Sell Stock with Cooldown

--- src/main/java/leetcode/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/leetcode/BestTimeToBuyAndSellStockWithCooldown.java
@@ -43,7 +43,7 @@ public class BestTimeToBuyAndSellStockWithCooldown {
 
         for (int i=2; i< len; i++) {
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0] - prices[i]);
         }
         return dp[len-1][0];
     }
