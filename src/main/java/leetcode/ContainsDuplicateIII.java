89e764bab36f9eef26f60a23101f89f6a61054b3
Varun Gupta <varun.gupta04@nagarro.com>
2021-01-08 19:53:26 +0530

    Contains Duplicate III

---
+++ src/main/java/leetcode/ContainsDuplicateIII.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.TreeSet;
+
+/*  220. Contains Duplicate III */
+
+/*  https://leetcode.com/problems/contains-duplicate-iii/   */
+
+/*
+*
+* Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3, t = 0
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1, t = 2
+Output: true
+Example 3:
+
+Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+Output: false
+*
+* */
+
+public class ContainsDuplicateIII {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (t < 0) {
+            return false;
+        }
+
+        // sliding window
+        TreeSet<Long> set = new TreeSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (!set.subSet((long) nums[i] - t, true, (long) nums[i] + t, true).isEmpty()) {
+                return true;
+            }
+
+            set.add((long) nums[i]);
+            if (set.size() > k) {
+                set.remove((long) nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
