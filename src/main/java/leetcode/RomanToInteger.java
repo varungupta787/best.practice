86d1501add317cd7575947d8f133f9a784f97af1
Varun Gupta <varun.gupta@airtel.com>
2022-09-30 11:06:36 +0530

    Roman to Integer

---
+++ src/main/java/leetcode/RomanToInteger.java
@@ -0,0 +1,88 @@
+package leetcode;
+
+/*  https://leetcode.com/problems/roman-to-integer/ */
+
+/*  13. Roman to Integer    */
+
+/*
+*
+* Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+*
+* */
+
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+        if(len == 1) {
+            return getValue(arr[0]);
+        }
+        int i=0, res = 0;
+        while(i<len-1) {
+            int v1 = getValue(arr[i]), v2 = getValue(arr[i+1]);
+            if(v1 < v2) {
+                res = res + v2-v1;
+                i+=2;
+            } else {
+                res += v1;
+                i++;
+            }
+        }
+        if(i<len) {
+            res += getValue(arr[i]);
+        }
+        return res;
+
+    }
+
+    private int getValue(char ch) {
+        int val = 0;
+        switch(ch) {
+            case 'I' : val = 1;  break;
+            case 'V' : val = 5; break;
+            case 'X' : val = 10; break;
+            case 'L' : val = 50; break;
+            case 'C' : val = 100; break;
+            case 'D' : val = 500; break;
+            case 'M' : val = 1000; break;
+
+        }
+        return val;
+    }
+}
